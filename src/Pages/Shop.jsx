82b5763a2eb190de3a27c3a1453615c740b2bc1c
ali-hamas <alihamasghurki4@gmail.com>
2024-03-20 17:39:26 +0500

    setup shop

---
+++ src/Pages/Shop.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Shop.css";
+import ProductsSection from "../Components/ProductsSection/ProductsSection";
+
+const Shop = () => {
+  return (
+    <>
+      <section className="section">
+        <div className="section-heading">
+          <a>Shop</a>
+        </div>
+        <div className="section-outer">
+          <div className="section-inner">
+            <ProductsSection api={`https://fakestoreapi.com/products/`} />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Shop;
