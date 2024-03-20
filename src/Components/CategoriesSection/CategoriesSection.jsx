951a7c92218a5a939e21d2854f002b2dd853098a
ali-hamas <alihamasghurki4@gmail.com>
2024-03-20 16:03:52 +0500

    add categories

--- src/Components/CategoriesSection/CategoriesSection.jsx
+++ src/Components/CategoriesSection/CategoriesSection.jsx
@@ -1,49 +1,42 @@
 import React from "react";
 import "./CategoriesSection.css";
-import MenCategoryImg from "../../../public/Assets/men-category.jpeg";
-import WomenCategoryImg from "../../../public/Assets/women-category.jpeg";
 import { Link } from "react-router-dom";
 
 const CategoriesSection = () => {
   return (
     <>
-      <div className="section-heading">
-        <a href="#categories">Categories</a>
-      </div>
-      <div className="section-inner">
-        <div className="category-inner">
-          <div className="single-category category-men">
-            <div className="single-category-inner">
-              <div className="category-name">Men Category</div>
-              <div className="category-desc">
-                Lorem ipsum dolor sit amet lorem ipum dolor sit amet ipsum dolor
-              </div>
-              <Link to={"/men"} className="btn fill-btn">
-                Shop Now
-              </Link>
+      <div className="category-inner">
+        <div className="single-category category-men">
+          <div className="single-category-inner">
+            <div className="category-name">Men's Collection</div>
+            <div className="category-desc">
+              Lorem ipsum dolor sit amet lorem ipum dolor sit amet ipsum dolor
             </div>
+            <Link to={"/men"} className="btn fill-btn">
+              Shop Now
+            </Link>
           </div>
-          <div className="single-category category-women">
-            <div className="single-category-inner">
-              <div className="category-name">Women Category</div>
-              <div className="category-desc">
-                Lorem ipsum dolor sit amet lorem ipum dolor sit amet ipsum dolor
-              </div>
-              <Link to={"/women"} className="btn fill-btn">
-                Shop Now
-              </Link>
+        </div>
+        <div className="single-category category-women">
+          <div className="single-category-inner">
+            <div className="category-name">Women's Collection</div>
+            <div className="category-desc">
+              Lorem ipsum dolor sit amet lorem ipum dolor sit amet ipsum dolor
             </div>
+            <Link to={"/women"} className="btn fill-btn">
+              Shop Now
+            </Link>
           </div>
-          <div className="single-category category-jewelry">
-            <div className="single-category-inner">
-              <div className="category-name">Jewelry Category</div>
-              <div className="category-desc">
-                Lorem ipsum dolor sit amet lorem ipum dolor sit amet ipsum dolor
-              </div>
-              <Link to={"/jewelry"} className="btn fill-btn">
-                Shop Now
-              </Link>
+        </div>
+        <div className="single-category category-jewelry">
+          <div className="single-category-inner">
+            <div className="category-name">Jewelery Collection</div>
+            <div className="category-desc">
+              Lorem ipsum dolor sit amet lorem ipum dolor sit amet ipsum dolor
             </div>
+            <Link to={"/jewelery"} className="btn fill-btn">
+              Shop Now
+            </Link>
           </div>
         </div>
       </div>
